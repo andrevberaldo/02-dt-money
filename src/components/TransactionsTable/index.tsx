538e9be87cb1bd15ac1587699b188540062fa0cc
andrevberaldo <andreberaldo@hotmail.com>
2022-11-06 12:59:53 -0300

    adjust currency and date format using Intl native api

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -38,9 +38,21 @@ function TransactionTable() {
                         return (
                             <tr key={transaction.id}>
                                 <td>{transaction.category}</td>
-                                <td className={transaction.type}>{`R$ ${transaction.amount}`}</td>
+                                <td className={transaction.type}>
+                                    {
+                                        new Intl.NumberFormat('pt-BR', {
+                                            style: 'currency',
+                                            currency: 'BRL'
+                                        }).format(transaction.amount)
+                                    }
+                                </td>
                                 <td>{transaction.type}</td>
-                                <td>{transaction.createdAt}</td>
+                                <td>
+                                {
+                                        new Intl.DateTimeFormat('pt-BR').format(
+                                            new Date(transaction.createdAt))
+                                    }
+                                </td>
                             </tr>
                         ) 
                     })}
