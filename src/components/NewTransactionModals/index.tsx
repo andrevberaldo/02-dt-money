a6fa177cfe62d04fdbeab4b43ab425b699f8df16
andrevberaldo <andreberaldo@hotmail.com>
2022-11-06 09:31:10 -0300

    a

---
+++ src/components/NewTransactionModals/index.tsx
@@ -0,0 +1,96 @@
+import Modal from 'react-modal';
+import { Container, RadioBox, TransactionTypeContainer } from './newTransactionModal.styles';
+import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg'
+import outcomeImg from '../../assets/outcome.svg'
+import { FormEvent, useState } from 'react';
+
+interface newTransactionModalProps {
+    isOpen: boolean;
+    onRequestClose: () => void;
+}
+
+export function NewTransactionModal({isOpen, onRequestClose}: newTransactionModalProps) {
+
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
+    const [type, setType] = useState('deposit');
+    
+    function handleCreateNewTransaction(props: FormEvent) {
+        props.preventDefault();
+
+        const data = {
+            title,
+            value,
+            category,
+            type
+        }
+        
+    }
+    
+    return (
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            overlayClassName={"react-modal-overlay"}
+            className={"react-modal-content"}
+        >
+            <Container
+                onSubmit={handleCreateNewTransaction}
+            >
+                <button
+                    className="react-modal-close" 
+                    type="button"
+                    onClick={onRequestClose}
+                    >                 
+                        <img src={closeImg} alt="closeButton" />
+                </button>
+                
+                <h2>New Transaction</h2>
+                
+                <input
+                    type="text"
+                    placeholder="Title"
+                    value={title}
+                    onChange={e => setTitle(e.target.value)}    
+                />
+                
+                <input 
+                    type="number" 
+                    placeholder="Value"
+                    value={value}
+                    onChange={e => setValue(Number(e.target.value))}    
+                />
+
+                <TransactionTypeContainer>
+                    <RadioBox
+                        onClick={() => {setType('deposit')}}
+                        isActive={type === 'deposit'}
+                        activeColor={'green'}
+                    >
+                        <img src={incomeImg} alt="Income" />
+                        <span>Income</span>
+                    </RadioBox>
+                    <RadioBox
+                        onClick={() => {setType('withdraw')}}
+                        isActive={type === 'withdraw'}
+                        activeColor={'red'}
+                    >
+                        <img src={outcomeImg} alt="outcome" />
+                        <span>Outcome</span>
+                    </RadioBox>
+                </TransactionTypeContainer>
+                
+                <input 
+                    type="text"
+                    placeholder="Category"
+                    value={category}
+                    onChange={e => setCategory(e.target.value)}    
+                />
+                
+                <button type="submit">Cadastrar</button>                
+            </Container>
+        </Modal>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { createServer } from 'miragejs';
+import { App } from './App';  
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amout: 400,
+          type: 'deposit',
+          category: 'Food',
+          creatAt: new Date().toLocaleDateString()
+        },
+        {
+          id: 2,
+          title: 'Transaction 1',
+          amout: -500,
+          type: 'withdraw',
+          category: 'Food',
+          creatAt: new Date().toLocaleDateString()
+        }
+      ]
+    });
+
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+      return data;
+    });
+  }
+})
 
 
 const root = ReactDOM.createRoot(
